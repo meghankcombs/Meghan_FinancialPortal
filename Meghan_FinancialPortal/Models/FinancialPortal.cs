d486a20106e48744887504cc27b915fbbed4d671
Meghan <meghankcombs@gmail.com>
2018-04-11 14:32:38 -0400

    Removed unused models and update connection string

--- Meghan_FinancialPortal/Models/FinancialPortal.cs
+++ Meghan_FinancialPortal/Models/FinancialPortal.cs
@@ -11,12 +11,7 @@ namespace Meghan_FinancialPortal.Models
             : base("name=FinancialPortal")
         {
         }
-
-        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
-        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
-        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
-        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
-        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+        
         public virtual DbSet<BudgetItem> BudgetItems { get; set; }
         public virtual DbSet<Budget> Budgets { get; set; }
         public virtual DbSet<Category> Categories { get; set; }
@@ -27,36 +22,6 @@ namespace Meghan_FinancialPortal.Models
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<AspNetRole>()
-                .HasMany(e => e.AspNetUsers)
-                .WithMany(e => e.AspNetRoles)
-                .Map(m => m.ToTable("AspNetUserRoles").MapLeftKey("RoleId").MapRightKey("UserId"));
-
-            modelBuilder.Entity<AspNetUser>()
-                .HasMany(e => e.AspNetUserClaims)
-                .WithRequired(e => e.AspNetUser)
-                .HasForeignKey(e => e.UserId);
-
-            modelBuilder.Entity<AspNetUser>()
-                .HasMany(e => e.AspNetUserLogins)
-                .WithRequired(e => e.AspNetUser)
-                .HasForeignKey(e => e.UserId);
-
-            modelBuilder.Entity<AspNetUser>()
-                .HasMany(e => e.Invites)
-                .WithOptional(e => e.AspNetUser)
-                .HasForeignKey(e => e.InvitedById);
-
-            modelBuilder.Entity<AspNetUser>()
-                .HasMany(e => e.PersonalAccounts)
-                .WithOptional(e => e.AspNetUser)
-                .HasForeignKey(e => e.CreatedById);
-
-            modelBuilder.Entity<AspNetUser>()
-                .HasMany(e => e.Transactions)
-                .WithOptional(e => e.AspNetUser)
-                .HasForeignKey(e => e.EnteredById);
-
             modelBuilder.Entity<Category>()
                 .HasMany(e => e.Households)
                 .WithMany(e => e.Categories)
